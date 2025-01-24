09b00e4d98546bff5c3499559378670c2452b53c
Raquel <raquel.melo.job@gmail.com>
2025-01-24 09:52:43 -0300

    Initial commit

---
+++ src/main/java/com/rbmelo/RealimgApplication.java
@@ -0,0 +1,13 @@
+package com.rbmelo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RealimgApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RealimgApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rbmelo/RealimgApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rbmelo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RealimgApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
